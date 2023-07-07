5315f15aeaf2a798d2c5e7f5eb4c982f5c3c45fa
shaw <1939484341@qq.com>
2023-07-07 09:46:42 +0800

    *

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.IO.Pipes;
+using System.IO;
+using System;
+using System.Windows;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            var text = MsgTextBox.Text;
+            using (NamedPipeClientStream pipeClient = new NamedPipeClientStream(".", "MyPipe", PipeDirection.Out))
+            {
+                pipeClient.Connect();
+
+                Console.WriteLine("Connected to server.");
+
+                using (StreamWriter writer = new StreamWriter(pipeClient))
+                {
+                    writer.AutoFlush = true;
+                    writer.WriteLine(text);
+                }
+            }
+        }
+    }
+}
---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.IO.Pipes;
+using System.IO;
+using System;
+using System.Windows;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            RecvMsg();
+        }
+
+        private void RecvMsg()
+        {
+            Console.WriteLine("Waiting for clients...");
+
+            using (NamedPipeServerStream pipeServer = new NamedPipeServerStream("MyPipe"))
+            {
+                pipeServer.WaitForConnection();
+
+                Console.WriteLine("Client connected.");
+
+                try
+                {
+                    using (StreamReader reader = new StreamReader(pipeServer))
+                    {
+                        string? message = reader.ReadLine();
+                        Console.WriteLine("Received message: " + message);
+                        MsgTextBlock.Text = message;
+                    }
+                }
+                catch (IOException e)
+                {
+                    Console.WriteLine("Error: {0}", e.Message);
+                }
+            }
+        }
+    }
+}
